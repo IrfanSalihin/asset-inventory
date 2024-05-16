364edf350738160c06159ec16736cd4426288f6c
IrfanSalihin <a179952@siswa.ukm.edu.my>
2024-05-16 12:22:46 +0800

    small changes

--- resources/views/desktop/show.blade.php
+++ resources/views/desktop/show.blade.php
@@ -1,4 +1,4 @@
-<!-- resources/views/desktop/view.blade.php -->
+<!-- resources/views/desktop/show.blade.php -->
 
 <x-app-layout>
     <x-slot name="header">
@@ -9,153 +9,58 @@
 
     <div class="py-12 bg-gray-100">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+            <div class="bg-white overflow-hidden shadow-lg sm:rounded-lg">
+                <div class="p-8 bg-white border-b border-gray-200">
                     <!-- Desktop Information -->
                     <div class="mb-8">
-                        <h4 class="text-2xl font-semibold mb-4">Desktop Information</h4>
-                        <div class="grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-2">
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Assigned Staff:</p>
-                                <p>{{ $desktop->assigned_staff_name }}</p>
+                        <h4 class="text-2xl font-semibold mb-4 text-gray-800">Desktop Information</h4>
+                        <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+                            @php
+                            $details = [
+                            'Assigned Staff' => 'assigned_staff_name',
+                            'Assigned Staff Number' => 'assigned_staff_number',
+                            'Location' => 'location',
+                            'Desktop Model' => 'desktop_model',
+                            'Desktop Name' => 'desktop_name',
+                            'Desktop Serial Number' => 'desktop_serial_number',
+                            'Monitor Model' => 'monitor_model',
+                            'Monitor Serial Number' => 'monitor_serial_number',
+                            'Keyboard' => 'keyboard',
+                            'Mouse' => 'mouse',
+                            'Power Cable' => 'power_cable',
+                            'Display Cable' => 'display_cable',
+                            'Operating System' => 'operating_system',
+                            'Windows Product Key' => 'windows_product_key',
+                            'Operating System Bit' => 'operating_system_bit',
+                            'Processor' => 'processor',
+                            'Device ID' => 'device_id',
+                            'Product ID' => 'product_id',
+                            'HDD Size (GB)' => 'hdd_sizes_gb',
+                            'SSD Size (GB)' => 'ssd_sizes_gb',
+                            'RAM Size (GB)' => 'ram_sizes_gb',
+                            'Microsoft Office Year' => 'microsoft_office_year',
+                            'Microsoft Office License' => 'microsoft_office_license',
+                            'Microsoft Office ID' => 'microsoft_office_id',
+                            'Microsoft Office Password' => 'microsoft_office_password',
+                            'Antivirus' => 'antivirus',
+                            'Antivirus Expired Date' => 'antivirus_expired_date',
+                            'Antivirus License' => 'antivirus_license',
+                            'Year Purchased' => 'year_purchased',
+                            'Account Date Purchase' => 'account_date_purchase',
+                            'Date Purchased' => 'date_purchased',
+                            'Price' => 'price',
+                            'Status' => 'status'
+                            ];
+                            @endphp
+
+                            @foreach ($details as $label => $attribute)
+                            @if (!in_array($attribute, ['antivirus_expired_date', 'antivirus_license']) || $desktop->antivirus === "Available")
+                            <div class="bg-gray-50 p-6 rounded-lg shadow-md border border-gray-200">
+                                <p class="text-lg font-semibold text-gray-800 mb-2">{{ $label }}:</p>
+                                <p class="text-gray-600">{{ $desktop->$attribute }}</p>
                             </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Assigned Staff Number:</p>
-                                <p>{{ $desktop->assigned_staff_number }}</p>
-                            </div>
-                            <!-- Add more sections for other desktop attributes -->
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Location:</p>
-                                <p>{{ $desktop->location }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Desktop Model:</p>
-                                <p>{{ $desktop->desktop_model }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Desktop Name:</p>
-                                <p>{{ $desktop->desktop_name }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Desktop Serial Number:</p>
-                                <p>{{ $desktop->desktop_serial_number }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Monitor Model:</p>
-                                <p>{{ $desktop->monitor_model }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Monitor Serial Number:</p>
-                                <p>{{ $desktop->monitor_serial_number }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Keyboard:</p>
-                                <p>{{ $desktop->keyboard }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Mouse:</p>
-                                <p>{{ $desktop->mouse }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Power Cable:</p>
-                                <p>{{ $desktop->power_cable }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Display Cable:</p>
-                                <p>{{ $desktop->display_cable }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Operating System:</p>
-                                <p>{{ $desktop->operating_system }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Windows Product Key:</p>
-                                <p>{{ $desktop->windows_product_key }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Operating System Bit:</p>
-                                <p>{{ $desktop->operating_system_bit }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Processor:</p>
-                                <p>{{ $desktop->processor }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Device ID:</p>
-                                <p>{{ $desktop->device_id }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Product ID:</p>
-                                <p>{{ $desktop->product_id }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">HDD Size (GB):</p>
-                                <p>{{ $desktop->hdd_sizes_gb }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">SSD Size (GB):</p>
-                                <p>{{ $desktop->ssd_sizes_gb }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">RAM Size (GB):</p>
-                                <p>{{ $desktop->ram_sizes_gb }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Microsoft Office Year:</p>
-                                <p>{{ $desktop->microsoft_office_year }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Microsoft Office License:</p>
-                                <p>{{ $desktop->microsoft_office_license }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Microsoft Office ID:</p>
-                                <p>{{ $desktop->microsoft_office_id }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Microsoft Office Password:</p>
-                                <p>{{ $desktop->microsoft_office_password }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Antivirus:</p>
-                                <p>{{ $desktop->antivirus }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Antivirus Expired Date:</p>
-                                @if ($desktop->antivirus === "Available")
-                                <p>{{ $desktop->antivirus_expired_date }}</p>
-                                @else
-                                <p>Not Available</p>
-                                @endif
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Antivirus License:</p>
-                                @if ($desktop->antivirus === "Available")
-                                <p>{{ $desktop->antivirus_license }}</p>
-                                @else
-                                <p>Not Available</p>
-                                @endif
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Year Purchased:</p>
-                                <p>{{ $desktop->year_purchased }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Account Date Purchase:</p>
-                                <p>{{ $desktop->account_date_purchase }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Date Purchased:</p>
-                                <p>{{ $desktop->date_purchased }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Price:</p>
-                                <p>{{ $desktop->price }}</p>
-                            </div>
-                            <div class="mb-4">
-                                <p class="text-lg font-semibold">Status:</p>
-                                <p>{{ $desktop->status }}</p>
-                            </div>
+                            @endif
+                            @endforeach
                         </div>
                     </div>
                 </div>
